90bf4a23593da4d7965dca66a2e94228ce61a98c
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-06-29 20:35:31 +0400

    adding chat view

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-neutral-50 gap-8">
       AI CHATBOT
-<input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border rounded-md bg-black"/>
+      <div className="py-2 px-3 w-full border rounded-md w-full flex-grow"></div>
+      <input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border rounded-md bg-black" />
     </main>
   )
 }
