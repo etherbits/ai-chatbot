e6c82b143c5c9347feaeaa180f5a733157dc4f42
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-04 22:42:41 +0400

    mapping messages

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ import { useState } from 'react';
 
 export default function Home() {
   const [prompt, setPrompt] = useState("")
+  const [messages, setMessages] = useState(["Hello"])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-neutral-50 gap-8">
       AI CHATBOT
-      <div className="py-2 px-3 w-full border rounded-md w-full flex-grow"></div>
+      <div className="py-2 px-3 w-full border rounded-md w-full flex-grow">{messages.map((message, i) => <p key={i}>{message}</p>)}</div>
       <input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border rounded-md bg-black" value={prompt} onChange={(e) => { setPrompt(e.target.value) }} />
     </main>
   )
