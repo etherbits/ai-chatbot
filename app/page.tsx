572867f11cfa0870d527eb41e0a5d935f177ba7e
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-06 23:07:06 +0400

    saving prompt on submit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-neutral-50 gap-8">
       AI CHATBOT
       <div className="py-2 px-3 w-full border border-slate-700 rounded-md w-full flex-grow">{messages.map((message, i) => <p key={i}>{message}</p>)}</div>
-      <input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border border-slate-700 text-slate-200 rounded-md bg-black" value={prompt} onChange={(e) => { setPrompt(e.target.value) }} />
+      <form onSubmit={() => setMessages([...messages, prompt])}>
+        <input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border border-slate-700 text-slate-200 rounded-md bg-black" value={prompt} onChange={(e) => { setPrompt(e.target.value) }} />
+      </form>
     </main>
   )
 }
