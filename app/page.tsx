20219678e31aa591ebc354daaed7c2961afd2a6e
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-10 21:01:58 +0400

    changeing text color

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-neutral-50 gap-8">
       <div className="py-2 px-3 w-full border border-slate-700 rounded-md flex-grow">{messages.map((message, i) => <p key={i}>{message}</p>)}</div>
       <form className="w-full" onSubmit={(e) => { setMessages([...messages, prompt]); e.preventDefault(); setPrompt(""); }}>
-        <input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border border-slate-700 text-slate-200 rounded-md bg-black" value={prompt} onChange={(e) => { setPrompt(e.target.value) }} />
+        <input type="text" placeholder="enter your message..." className="py-2 px-3 w-full border border-slate-700 text-slate-300 rounded-md bg-black" value={prompt} onChange={(e) => { setPrompt(e.target.value) }} />
       </form>
     </main>
   )
